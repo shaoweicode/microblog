4a09aa80e785b8267fb8ce2fa12c6c39d4a0e8c9
shaoweicode <417096554@qq.com>
2018-03-29 16:39:09 +0800

    数据库完成

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ OPENID_PROVIDERS=[
     {'name':'AOL','url':'https://www.flicker.com/<username>'},
     {'name':'MyOpenID','url':'https://www.openid.com'}
 ]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir,'db_respository')
